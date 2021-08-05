0334aaea201219da7d7321151f662365300fd4d4
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-08-05 18:09:08 +0800

    Add files via upload

---
+++ data/PoIRembang_2.js
@@ -0,0 +1,17 @@
+var json_PoIRembang_2 = {
+"type": "FeatureCollection",
+"name": "PoIRembang_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Klenteng Cu An Kiong | Abad 13M", "desc": "Tjoe An Kiong Temple, Jl. Dasun No.19, Pereng, Soditan, Lasem, Rembang Regency, Central Java 59271, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 111.4438628, -6.6943835 ] } },
+{ "type": "Feature", "properties": { "name": "Yopia Kupu Kupu", "desc": "Yopia Kupu Kupu, Karangturi, Lasem, Rembang Regency, Central Java 59271, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 111.4440039, -6.7020705 ] } },
+{ "type": "Feature", "properties": { "name": "Waroeng Lasem", "desc": "Waroeng Lasem, Mahbong, Karangturi, Kec. Lasem, Kabupaten Rembang, Jawa Tengah 59271, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 111.4467205, -6.7001508 ] } },
+{ "type": "Feature", "properties": { "name": "Oemah Batik Lasem", "desc": "Oemah Batik Lasem, Mahbong, Karangturi, Kec. Lasem, Kabupaten Rembang, Jawa Tengah 59271, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 111.4471095, -6.7001329 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Jami' Baiturrahman Lasem 1588 M", "desc": "Jami' Baiturrahman Lasem Mosque, Jalan Provinsi Lasem - Sale, Kauman, Mahbong, Karangturi, Kec. Lasem, Kabupaten Rembang, Jawa Tengah 59271, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 111.4474318, -6.698042 ] } },
+{ "type": "Feature", "properties": { "name": "RSI Arafah", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 111.4031385, -6.697719 ] } },
+{ "type": "Feature", "properties": { "name": "Polsek Lasem", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 111.4393763, -6.6956494 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU 44.592.18", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 111.429677, -6.6939695 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Jami' Baiturrahman Lasem", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 111.4441237, -6.698008 ] } },
+{ "type": "Feature", "properties": { "name": "RSUD Dr. R. Soetrasno", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 111.356008, -6.7089339 ] } }
+]
+}
